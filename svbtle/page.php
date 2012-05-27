fc9bd44d1115da59966d9df3dd1dc28e97df0d58
Joseph Scavone <josephscavone@gmail.com>
2012-05-27 18:24:19 -0500

    Page template created

---
+++ svbtle/page.php
@@ -0,0 +1,19 @@
+<?php get_header(); ?>
+<?php $options = get_option ( 'svbtle_options' ); ?>
+<?php global $wp_query; $total_pages = $wp_query->max_num_pages; if ( $total_pages > 1 ) { ?>
+<?php } ?>
+<?php while ( have_posts() ) : the_post() ?>
+		<article id="<?php the_ID(); ?>">
+			<h2>
+				<a href="<?php the_permalink(); ?>" ><?php the_title(); ?></a>
+				<?php if ( $options['anchor'] ) { ?><span class="anchor"><a href="<?php echo home_url('/'); ?>?p=<?php the_ID(); ?>">&#9875;</a></span><?php } ?>
+			</h2>
+			<?php the_content( __( 'Continue reading <span class="meta-nav">&raquo;</span>', 'hbd-theme' )  ); ?>
+			
+			<h1><?php //echo get_the_category_list(', '); ?></h1>
+		</article>
+<?php endwhile; ?>
+<footer id="paginate">
+	<?php single_content_nav( 'nav-below' ); ?>
+</footer>
+<?php get_footer(); ?>
